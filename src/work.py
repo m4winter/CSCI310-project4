5927dec61f528fe106614e43366c70912a81e39c
Jude Zalme <jz0008@morningside.edu>
2025-08-28 15:50:56 -0500

    python file

--- src/work.py
+++ src/work.py
@@ -12,12 +12,24 @@ def myUpperCase(mystring):
     str
         an upper case version of my string word
     """
+    """
+    The program deals with math and other list type stuff.
+    parameters
+    ------
+    nothing
+        there is nothing that you enter that would be in the program.
+    returns
+    -------
+    lists and other like it
+        like it says it is just normal list and stuff like that, that are in the program.
+    """
     return mystring.upper()
 if __name__ == "__main__":
     print("hello world")
     mstr = "this is a testing area for a function"
     ustr = myUpperCase(mstr)
     print(ustr)
+    print("1. 64 squared - below")
     sq = mt.sqrt(64)
     print(sq)
     mathlist = [0] * 11
@@ -32,11 +44,28 @@ if __name__ == "__main__":
     mathlist[8] = 10//20
     mathlist[9] = 4%8
     mathlist[10] = 5**40
+    print("2. Math List is below")
     print(mathlist)
+    print("2.1. Mathlist with the first number is changed. - below")
+    mathlist.insert(0,321)
+    print("3. Words with numbers below")
     justalist = ["words","are","not","1n","h3r3"]
     print(justalist)
+    print("How much words in list - below")
+    print(len(justalist))
+    print("3.1. List above times 2 - below")
     listbutmore = [justalist]*2
     print(listbutmore)
-    mathlist.insert(0,321)
+    print("3.2. one words from list")
+    oneword = justalist[2]
+    print(oneword)
+    print("3.3. one words but in all caps")
+    print(oneword.upper())
+    print("4. Range lists with different variants")
     print(mathlist)
-    
+    list1 = list(range(10))
+    print(list1)
+    list2 = list(range(5,10))
+    print(list2)
+    list3 = list(range(20,0,-1))
+    print(list3)
